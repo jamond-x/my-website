418177a3abc406883fcff3b648e5f734007a8eeb
xiangxingqiang <xiangxingqiang@zhihu.com>
2022-07-04 22:42:01 +0800

    add blogs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,9 +67,9 @@ const config = {
             type: "doc",
             docId: "intro",
             position: "left",
-            label: "Tutorial",
+            label: "文章",
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          { to: "/blog", label: "博客", position: "left" },
           {
             href: "https://github.com/jamond-x",
             label: "GitHub",
