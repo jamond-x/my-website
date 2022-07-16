7a9d221e8dc74fd8b37f3c68f3891283129f74e3
xiangxingqiang <xiangxingqiang@zhihu.com>
2022-07-16 09:28:11 +0800

    blog: 我的困境

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,55 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Hueng's Site",
-  tagline: "生活沉淀站",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  tagline: '生活沉淀站',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "jamond-x", // Usually your GitHub org/user name.
-  projectName: "jamond-x.github.io", // Usually your repo name.
+  organizationName: 'jamond-x', // Usually your GitHub org/user name.
+  projectName: 'jamond-x.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          blogSidebarTitle: '全部博文',
+          blogSidebarCount: 'ALL',
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -59,63 +61,63 @@ const config = {
       navbar: {
         title: "Hueng's Site",
         logo: {
-          alt: "My Site Logo",
-          src: "img/logo.svg",
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "文章",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '文章',
           },
-          { to: "/blog", label: "博客", position: "left" },
+          { to: '/blog', label: '博客', position: 'left' },
           {
-            href: "https://github.com/jamond-x",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/jamond-x',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "相关链接",
+            title: '相关链接',
             items: [
               {
-                label: "Github",
-                href: "https://github.com/jamond-x",
+                label: 'Github',
+                href: 'https://github.com/jamond-x',
               },
               {
-                label: "邮箱",
-                href: "mailto:huengemail@163.com",
+                label: '邮箱',
+                href: 'mailto:huengemail@163.com',
               },
             ],
           },
           {
-            title: "社区",
+            title: '社区',
             items: [
               {
-                label: "FWF工作室",
-                href: "http://404fwf.cn/",
+                label: 'FWF工作室',
+                href: 'http://404fwf.cn/',
               },
               {
-                label: "0xFFFF社区",
-                href: "http://0xffff.one/",
+                label: '0xFFFF社区',
+                href: 'http://0xffff.one/',
               },
             ],
           },
           {
-            title: "更多",
+            title: '更多',
             items: [
               {
-                label: "力扣1",
-                href: "https://leetcode.cn/u/hueng_/",
+                label: '力扣1',
+                href: 'https://leetcode.cn/u/hueng_/',
               },
               {
-                label: "力扣2",
-                href: "https://leetcode.cn/u/jin-xi-jamond/",
+                label: '力扣2',
+                href: 'https://leetcode.cn/u/jin-xi-jamond/',
               },
             ],
           },
